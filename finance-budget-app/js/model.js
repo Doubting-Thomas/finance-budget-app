e2b2ca8718bf2d27eab85d0d087fd28296e2d002
Doubting-Thomas <thomasmichaelayo@gmail.com>
2022-12-24 06:08:10 -0800

    feat: add login functionality

--- finance-budget-app/js/model.js
+++ finance-budget-app/js/model.js
@@ -1,5 +1,70 @@
+const btnLogin = document.querySelector(".btn--login");
+const userLogin = document.querySelector(".login__user");
+const pinLogin = document.querySelector(".login__pin");
+const messageLogin = document.querySelector(".cta__text");
+const userDisplay = document.querySelector(".container");
+const formLogin = document.querySelector(".login__input");
+
+// Account details for login
+const firstAccount = {
+  username: "John Doe",
+  password: 5467,
+};
+
+const secondAccount = {
+  username: "Mary Potter",
+  password: 3421,
+};
+
+const userAccounts = [firstAccount, secondAccount];
+
+btnLogin.addEventListener("click", function (e) {
+  e.preventDefault();
+  let currentUserAccount;
+  // Find the username
+  currentUserAccount = userAccounts.find(function (acc) {
+    return acc.username === "John Doe";
+  });
+  console.log(currentUserAccount);
+
+  // Check for the PIN
+  if (currentUserAccount?.password === Number(pinLogin.value)) {
+    userDisplay.style.opacity = 100;
+
+    messageLogin.textContent = `Welcome, ${currentUserAccount.username}`;
+
+    // Clear input fields
+    userLogin.value = "";
+    pinLogin.value = "";
+    init();
+  }
+});
+
+// // Find the username
+// currentUserAccount = userAccounts.find(function (cur) {
+//   cur.username === this.userLogin.value;
+// });
+
+// // Check for the PIN
+// if (currentUserAccount?.pin === Number(this.pinLogin.value)) {
+//   this.userDisplay.style.opacity = 100;
+
+//   this.messageLogin.textContent = `Welcome, ${
+//     currentUserAccount.username.split("")[0]
+//   }`;
+
+//   // Clear input fields
+//   this.userLogin.value = this.pinLogin.value = "";
+
 class Budget {
   constructor() {
+    // this.userLogin = document.querySelector(".login__user");
+    // this.pinLogin = document.querySelector(".login__pin");
+    // this.btnLogin = document.querySelector(".btn--login");
+    // this.messageLogin = document.querySelector(".cta__text");
+    // this.userDisplay = document.querySelector(".container");
+    // this.formLogin = document.querySelector(".login__input");
+
     this.budgetInformation = document.querySelector(".budget__info");
     this.expenseInformation = document.querySelector(".expense__info");
     this.formBudget = document.querySelector(".form__budget");
@@ -11,6 +76,40 @@ class Budget {
     this.formExpense = document.querySelector(".expense__form");
     this.inputAmount = document.querySelector(".input-amount");
   }
+
+  // Login functionality
+  // loginProcess() {
+  //   let currentUserAccount;
+
+  //   const firstAccount = {
+  //     username: "John Doe",
+  //     password: 5467,
+  //   };
+
+  //   const secondAccount = {
+  //     username: "Mary Potter",
+  //     password: 3421,
+  //   };
+
+  //   const userAccounts = [firstAccount, secondAccount];
+
+  //   // Find the username
+  //   currentUserAccount = userAccounts.find(function (cur) {
+  //     cur.username === this.userLogin.value;
+  //   });
+
+  //   // Check for the PIN
+  //   if (currentUserAccount?.pin === Number(this.pinLogin.value)) {
+  //     this.userDisplay.style.opacity = 100;
+
+  //     this.messageLogin.textContent = `Welcome, ${
+  //       currentUserAccount.username.split("")[0]
+  //     }`;
+
+  //     // Clear input fields
+  //     this.userLogin.value = this.pinLogin.value = "";
+  //   }
+  // }
 
   // Submit the budget on command
   submitFormBudget() {
@@ -83,8 +182,14 @@ class Budget {
 function init() {
   const formBudget = document.querySelector(".form__budget");
   const formExpense = document.querySelector(".expense__form");
+  const formLogin = document.querySelector(".login__input");
 
   const budget = new Budget();
+
+  // formLogin.addEventListener("submit", function (e) {
+  //   e.preventDefault();
+  //   budget.loginProcess();
+  // });
 
   // Submit budget form
   formBudget.addEventListener("submit", function (e) {
@@ -98,4 +203,30 @@ function init() {
     budget.submitFormExpense();
   });
 }
-init();
+
+// const btnLogin = document.querySelector(".btn--login");
+// const formLogin = document.querySelector(".login__input");
+
+// function loginUser() {
+//   const budget = new Budget();
+//   budget.loginProcess();
+// }
+
+// function formLoginProcess() {
+//   const budget = new Budget();
+//   budget.loginProcess();
+// }
+
+// formLogin.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   formLoginProcess();
+// });
+
+// btnLogin.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   loginUser();
+// });
+// function loginUser() {
+//   const budget = new Budget();
+//   budget.loginProcess();
+// }
