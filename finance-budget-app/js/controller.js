ad6917a0e866f3da4086194e2f8370417a5d4a40
Doubting-Thomas <thomasmichaelayo@gmail.com>
2022-12-26 01:34:41 -0800

    fix: remove unused global variable

--- finance-budget-app/js/controller.js
+++ finance-budget-app/js/controller.js
@@ -4,5 +4,4 @@ export const userLogin = document.querySelector(".login__user");
 export const pinLogin = document.querySelector(".login__pin");
 export const messageLogin = document.querySelector(".cta__text");
 export const userDisplay = document.querySelector(".container");
-export const formLogin = document.querySelector(".login__input");
 export const logoutText = document.querySelector(".cta__text");
--- finance-budget-app/js/model.js
+++ finance-budget-app/js/model.js
@@ -5,7 +5,6 @@ import {
   pinLogin,
   messageLogin,
   userDisplay,
-  formLogin,
   logoutText,
 } from "./controller.js";
 
