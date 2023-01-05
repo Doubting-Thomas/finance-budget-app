c4131a7fe43bb3c390e12f527e9e77ff4557156b
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-01-05 12:30:45 -0800

    chore: remove unused code

--- finance-budget-app/js/model.js
+++ finance-budget-app/js/model.js
@@ -39,14 +39,6 @@ btnLogin.addEventListener("click", function (e) {
     btnLogout.classList.remove("hidden");
     init();
   } else {
-    // // inputLogin.style.border = "1px solid red";
-    // // pinLogin.style.border =
-    // inputLogin.forEach(function (input) {
-    //   input.addEventListener("click", function () {
-    //     //e.preventDefault();
-    //     console.log(input);
-    //   });
-    // });
     userLogin.classList.add("error");
     pinLogin.classList.add("error");
 
